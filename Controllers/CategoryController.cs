72ef97e7053221d1f0c251a9cc19cdff8f5fa9d6
Gustavo Borges <gustavo.borges.carvalho@hotmail.com>
2020-05-02 14:43:45 -0300

    Added ICategoryRepository dependency to CategoryController

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,65 +1,137 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using ProductCatalog.Data;
 using ProductCatalog.Models;
+using ProductCatalog.Repositories.Contracts;
+using ProductCatalog.ViewModels;
+using ProductCatalog.ViewModels.CategoryViewModels;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace ProductCatalog.Controllers
 {
     public class CategoryController : Controller
     {
-        private readonly StoreDataContext _context;
+        private readonly ICategoryRepository _categoryRepository;
 
-        public CategoryController(StoreDataContext context)
+        public CategoryController(ICategoryRepository categoryRepository)
         {
-            _context = context;
+            _categoryRepository = categoryRepository;
         }
 
         [Route("v1/categories/{id}")]
         [HttpGet]
         public Category GetCategory(int id)
         {
-            return _context.Categories.AsNoTracking().Where(x=> x.Id == id).FirstOrDefault();
+            return _categoryRepository.Get(id);
         }
+
+        [Route("v1/categories")]
+        [HttpGet]
+        public IEnumerable<Category> GetCategory()
+        {
+            return _categoryRepository.Get();
+        }
+
 
         [Route("v1/categories/{id}/products")]
         [HttpGet]
         public IEnumerable<Product> GetProducts(int id)
         {
-            return _context.Products.AsNoTracking().Where(x => x.CategoryId == id).ToList();
+            return _categoryRepository.GetProducts(categoryId: id);
         }
+
 
         [Route("v1/categories")]
         [HttpPost]
-        public Category Post([FromBody]Category category)
+        public ResultViewModel Post([FromBody] EditorCategoryViewModel model)
         {
-            _context.Categories.Add(category);
-            _context.SaveChanges();
+            model.Validate();
 
-            return category;
+            if (model.Invalid)
+            {
+                return new ResultViewModel()
+                {
+                    Success = false,
+                    Message = "Não foi possível criar categoria",
+                    Data = model
+                };
+            }
+
+            var category = new Category() { Title = model.Title };
+
+            category = _categoryRepository.Create(category);
+
+            return new ResultViewModel()
+            {
+                Success = true,
+                Message = "Categoria criada com sucesso",
+                Data = category
+            };
         }
 
-        
+
         [Route("v1/categories")]
         [HttpPut]
-        public Category Put([FromBody]Category category)
+        public ResultViewModel Put([FromBody] EditorCategoryViewModel model)
         {
-            _context.Categories.Update(category);
-            _context.SaveChanges();
+            model.Validate();
+            if (model.Invalid)
+            {
+                return new ResultViewModel()
+                { 
+                    Success = false,
+                    Message = "Não foi possível alterar categoria",
+                    Data = model 
+                };
+            }
 
-            return category;
+            var category = _categoryRepository.Get(model.Id);
+            if (category is null)
+            {
+                return new ResultViewModel()
+                { 
+                    Success = false,
+                    Message = "Categoria não encontrada",
+                    Data = model
+                };
+            }
+
+            category.Title = model.Title;
+
+            _categoryRepository.Update(category);
+
+
+            return new ResultViewModel()
+            {
+                Success = true,
+                Message = "Categoria alterada com sucesso",
+                Data = category
+            };
         }
 
-        
+
         [Route("v1/categories")]
         [HttpDelete]
-        public Category Delete([FromBody]Category category)
+        public ResultViewModel Delete([FromBody] EditorCategoryViewModel model)
         {
-            _context.Categories.Remove(category);
-            _context.SaveChanges();
+            var category = _categoryRepository.Get(model.Id);
 
-            return category;
+            if (category is null)
+            {
+                return new ResultViewModel()
+                {
+                    Success = false,
+                    Message = "Não foi possível deletar este item",
+                    Data = model
+                };
+            }
+
+            _categoryRepository.Delete(category);
+
+            return new ResultViewModel()
+            {
+                Success = true,
+                Message = "Produto deletado com sucesso.",
+                Data = category
+            };
         }
     }
 }
